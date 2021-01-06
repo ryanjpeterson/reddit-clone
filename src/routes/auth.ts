e49215312071f0adb45ccb63b1c86fa3767b3c66
unknown <ryanjay333@gmail.com>
2021-01-06 10:29:22 -0500

    added typescript config in lesson 5

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -54,7 +54,7 @@ const login = async (req: Request, res: Response) => {
 
         if (!passwordMatches) res.status(401).json({ password: 'Password is incorrect!'})
 
-        const token = jwt.sign({ username }, process.env.JWT_SECRET)
+        const token = jwt.sign({ username }, process.env.JWT_SECRET!)
 
         res.set('Set-Cookie', cookie.serialize('token', token, {
             httpOnly: true,
@@ -65,7 +65,8 @@ const login = async (req: Request, res: Response) => {
         }))
         return res.json(user)
     } catch (err) {
-        
+        console.log(err)
+        return res.json({ error: 'Something Went Wrong!' })
     }
 }
 
