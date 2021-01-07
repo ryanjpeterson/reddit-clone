0c6217df9661b22d5b377c72286d50b60d880d58
unknown <ryanjay333@gmail.com>
2021-01-07 16:28:46 -0500

    finished lesson 8

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,25 @@
-import '../styles/globals.css';
+import { Fragment } from 'react';
 import { AppProps } from 'next/app';
 import axios from 'axios';
+
+import '../styles/tailwind.css';
+
+import Navbar from '../components/Navbar';
+import { useRouter } from 'next/router';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ['/register', '/login'];
+  const authRoute = authRoutes.includes(pathname);
+  return (
+    <Fragment>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default App;
